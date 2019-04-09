7b72b7a807c81308398a27851df614cfcaebcab0
Ayaz Akram <yazakram@ucdavis.edu>
2019-04-08 22:32:47 -0700

    Add HW1 along with a template

---
+++ HW1/template/serial_ocean.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void ocean(int **grid, int xdim, int ydim, int timesteps)
+{
+	/********************* the red-black algortihm (start)************************/
+	/*
+    In odd timesteps, calculate indeces with - and in even timesteps, calculate indeces with * 
+    See the example of 6x6 matrix, A represents the corner elements. 
+        A A A A A A
+        A - * - * A
+        A * - * - A
+        A - * - * A
+        A * - * - A
+        A A A A A A 
+    */
+
+	// PUT YOUR CODE HERE
+	
+	/////////////////////// the red-black algortihm (end) ///////////////////////////
+}
